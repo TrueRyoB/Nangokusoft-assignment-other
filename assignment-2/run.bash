c57184953088b673321281712b4e25a92f73aac7
TrueRyoB <121699111+TrueRyoB@users.noreply.github.com>
2025-04-08 13:27:08 -0500

    Fixed exe file name

--- assignment-2/run.bash
+++ assignment-2/run.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-g++ -g -std=c++17 -Wall -Werror main.cpp vector2.cpp ask.cpp config.cpp -o trueryob -fsanitize=address
-./trueryob $*
+g++ -g -std=c++17 -Wall -Werror main.cpp vector2.cpp ask.cpp config.cpp -o rotate -fsanitize=address
+./rotate $*
