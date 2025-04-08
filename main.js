31e98dd8436839489c0bdb8f990f4ea71977c203
TrueRyoB <121699111+TrueRyoB@users.noreply.github.com>
2025-04-08 15:52:54 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ document.getElementById("btn").addEventListener("click", function () {
   alert("外部ファイルからこんにちは！");
 });
 
-document.getElementById("tweetBtn").addEventListener("click", function () {
+document.getElementById("reportBtn").addEventListener("click", function () {
   const text = encodeURIComponent("#2025電卓　(開発者はこのタグを不定期に検索することでデバッグに取り掛かります！)");
   const url = `https://twitter.com/intent/tweet?text=${text}`;
   window.open(url, "_blank");
